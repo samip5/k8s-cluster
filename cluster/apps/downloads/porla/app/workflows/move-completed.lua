25987124fb7f183ab5a90120f8047e7ba765b7bf
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-07-27 18:09:35 +0300

    Porla: Update workflows

--- cluster/apps/downloads/porla/app/workflows/move-completed.lua
+++ cluster/apps/downloads/porla/app/workflows/move-completed.lua
@@ -1,31 +1,35 @@
-local Workflow          = require "porla.Workflow"
-local TorrentFinished   = require "porla.triggers.TorrentFinished"
-local TorrentFlags      = require "porla.actions.TorrentFlags"
-local TorrentMove       = require "porla.actions.TorrentMove"
+local torrents  = require("torrents")
+local workflows = require("workflows")
+local torrent   = require("workflows.actions.torrent")
+local finished  = require("workflows.triggers.torrent").finished
 
-return Workflow:new{
-    on = TorrentFinished:new{},
-    actions = {
-        TorrentFlags:new{
-            unset = {
-                "auto_managed"
-            }
-        },
-        TorrentMove:new{
-            path = function(ctx)
-                local category = ctx.torrent.category or "default"
-                if(category == "movies") then
-                    return "/media/downloads/porla/Movies"
-                elseif(category == "sonarr") then
-                    return "/media/downloads/porla/Sonarr"
-                elseif(category == "radarr") then
-                    return "/media/downloads/porla/Radarr"
-                elseif(category == "default") then
-                    return "/media/downloads/porla/"
-                elseif(ctx.torrent.save_path == "/media/downloads/porla/") then
-                    return "/media/downloads/porla/"..category
+function porla.init()
+    workflows.add({
+        on = finished(),
+        actions = {
+            function(ctx, callback)
+                local props = torrents.properties.get(ctx.torrent)
+                props.flags = props.flags & ~torrents.flags.auto_managed
+                torrents.properties.set(ctx.torrent, props)
+
+                callback()
+            end,
+            torrent.move({
+                path = function(ctx)
+                    local category = ctx.torrent.category or "default"
+                    if(category == "movies") then
+                        return "/media/downloads/porla/Movies"
+                    elseif(category == "sonarr") then
+                        return "/media/downloads/porla/Sonarr"
+                    elseif(category == "radarr") then
+                        return "/media/downloads/porla/Radarr"
+                    elseif(category == "default") then
+                        return "/media/downloads/porla/"
+                    elseif(ctx.torrent.save_path == "/media/downloads/porla/") then
+                        return "/media/downloads/porla/"..category
+                    end
                 end
-            end
+            })
         }
-    }
-}
+    })
+end
