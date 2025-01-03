28db7908648d9dddcdfedf7313011202270b66db
Skyler Mäntysaari <samip5@users.noreply.github.com>
2025-01-03 09:23:03 +0200

    Oops

--- terraform/mikrotik/modules/rb5009/bgp/main.tf
+++ terraform/mikrotik/modules/rb5009/bgp/main.tf
@@ -8,12 +8,17 @@ terraform {
 
 resource "routeros_routing_bgp_template" "default" {
   as   = "213021"
-  name = "default"
+  name = "better"
+  address_families = "ip, ipv6"
 }
 
 resource "routeros_routing_bgp_connection" "plex-srv-v6" {
   as   = routeros_routing_bgp_template.default.as
   name = "plex-srv-v6"
+  address_families = "ipv6"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "fd9d:7a72:44eb:a:a236:9fff:fe18:55fb"
     as = routeros_routing_bgp_template.default.as
@@ -23,6 +28,11 @@ resource "routeros_routing_bgp_connection" "plex-srv-v6" {
 resource "routeros_routing_bgp_connection" "plex-srv-v4" {
   as   = routeros_routing_bgp_template.default.as
   name = "plex-srv-v4"
+  address_families = "ip"
+  local {
+    role = "ibgp"
+  }
+
   remote {
     address = "192.168.2.129"
     as = routeros_routing_bgp_template.default.as
--- terraform/mikrotik/modules/rb5009/bgp/prod-cluster.tf
+++ terraform/mikrotik/modules/rb5009/bgp/prod-cluster.tf
@@ -2,6 +2,12 @@ resource "routeros_routing_bgp_connection" "nebula-m1-v4" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-m1"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ip"
+
+  local {
+    role = "ibgp"
+  }
+
   remote {
     address = "10.0.105.34"
     as = routeros_routing_bgp_template.default.as
@@ -13,6 +19,10 @@ resource "routeros_routing_bgp_connection" "nebula-m1-v6" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-m1-v6"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ipv6"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "fd9d:7a72:44eb:c:6e4b:90ff:fe2d:a7dd"
     as = routeros_routing_bgp_template.default.as
@@ -23,6 +33,10 @@ resource "routeros_routing_bgp_connection" "nebula-w-amd-1-v4" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w-amd-1-v4"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ip"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "10.0.105.50"
     as = routeros_routing_bgp_template.default.as
@@ -34,6 +48,10 @@ resource "routeros_routing_bgp_connection" "nebula-w-amd-1-v6" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w-amd-1-v6"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ipv6"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "fd9d:7a72:44eb:c:6e4b:90ff:fe2d:a807"
     as = routeros_routing_bgp_template.default.as
@@ -44,6 +62,10 @@ resource "routeros_routing_bgp_connection" "nebula-w-amd-2-v4" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w-amd-2-v4"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ip"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "10.0.105.50"
     as = routeros_routing_bgp_template.default.as
@@ -55,6 +77,10 @@ resource "routeros_routing_bgp_connection" "nebula-w-amd-2-v6" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w-amd-2-v6"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ipv6"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "fd9d:7a72:44eb:c:6e4b:90ff:fe2d:a807"
     as = routeros_routing_bgp_template.default.as
@@ -65,6 +91,10 @@ resource "routeros_routing_bgp_connection" "nebula-w-amd-3-v4" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w-amd-3-v4"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ip"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "10.0.105.33"
     as = routeros_routing_bgp_template.default.as
@@ -76,6 +106,10 @@ resource "routeros_routing_bgp_connection" "nebula-w-amd-3-v6" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w-amd-3-v6"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ipv6"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "fd9d:7a72:44eb:c:2d0:b4ff:fe02:197"
     as = routeros_routing_bgp_template.default.as
@@ -86,6 +120,10 @@ resource "routeros_routing_bgp_connection" "nebula-w-amd-4-v4" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w-amd-4-v4"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ip"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "10.0.105.28"
     as = routeros_routing_bgp_template.default.as
@@ -97,6 +135,10 @@ resource "routeros_routing_bgp_connection" "nebula-w-amd-4-v6" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w-amd-4-v6"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ipv6"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "fd9d:7a72:44eb:c:dacb:8aff:fe1d:a0d2"
     as = routeros_routing_bgp_template.default.as
@@ -108,6 +150,10 @@ resource "routeros_routing_bgp_connection" "nebula-cm4-1-v4" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-cm4-1-v4"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ip"
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "10.0.105.32"
     as = routeros_routing_bgp_template.default.as
@@ -120,6 +166,11 @@ resource "routeros_routing_bgp_connection" "nebula-cm4-1-v6" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-cm4-1-v6"
   as = routeros_routing_bgp_template.default.as
+  address_families = "ipv6"
+
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "fd9d:7a72:44eb:c:e65f:1ff:fe2e:6ac"
     as = routeros_routing_bgp_template.default.as
@@ -130,6 +181,12 @@ resource "routeros_routing_bgp_connection" "nebula-w7-v4" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w7-v4"
   as = routeros_routing_bgp_template.default.as
+
+  address_families = "ip"
+
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "10.0.105.29"
     as = routeros_routing_bgp_template.default.as
@@ -142,6 +199,12 @@ resource "routeros_routing_bgp_connection" "nebula-w7-v6" {
   templates = [routeros_routing_bgp_template.default.name]
   name = "nebula-w7-v6"
   as = routeros_routing_bgp_template.default.as
+
+  address_families = "ipv6"
+
+  local {
+    role = "ibgp"
+  }
   remote {
     address = "fd9d:7a72:44eb:c:da3a:ddff:fed8:70bc"
     as = routeros_routing_bgp_template.default.as
