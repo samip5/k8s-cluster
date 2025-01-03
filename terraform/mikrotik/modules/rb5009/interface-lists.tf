3b636095523ca430956f8611739604c2667512be
Skyler Mäntysaari <samip5@users.noreply.github.com>
2025-01-03 10:13:04 +0200

    Now it's in prod

--- terraform/mikrotik/modules/rb5009/interface-lists.tf
+++ terraform/mikrotik/modules/rb5009/interface-lists.tf
@@ -7,3 +7,18 @@ resource "routeros_interface_list_member" "WAN2" {
   interface = routeros_interface_ethernet.wan2.name
   list = "WAN"
 }
+
+resource "routeros_interface_list_member" "VLANLAN" {
+  interface = routeros_interface_vlan.LAN.name
+  list = "LAN"
+}
+
+resource "routeros_interface_list_member" "KubeProd" {
+  interface = routeros_interface_vlan.KubeProd.name
+  list = "LAN"
+}
+
+resource "routeros_interface_list_member" "KubeDev" {
+  interface = routeros_interface_vlan.KubeDev.name
+  list = "LAN"
+}
