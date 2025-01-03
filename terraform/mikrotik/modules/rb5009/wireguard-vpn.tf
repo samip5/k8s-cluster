4f0c423c279f862ff3aa7e9e54ecc503b90505e6
Skyler Mäntysaari <sm+git@skym.fi>
2025-01-03 23:19:15 +0200

    Fixing up Wireguard

--- terraform/mikrotik/modules/rb5009/wireguard-vpn.tf
+++ terraform/mikrotik/modules/rb5009/wireguard-vpn.tf
@@ -23,3 +23,25 @@ resource "routeros_interface_wireguard_peer" "backdoor-peer1" {
   persistent_keepalive = "1520s"
   is_responder = true
 }
+
+resource "routeros_ip_address" "backdoorV4" {
+  address   = "10.13.38.4/32"
+  interface = routeros_interface_wireguard.backdoor.name
+}
+
+resource "routeros_ipv6_address" "backdoorV6" {
+  address   = "2001:67c:1be8:3b::4/12"
+  interface = routeros_interface_wireguard.backdoor.name
+}
+
+resource "routeros_ip_firewall_nat" "backdoorNAT" {
+  action = "masquerade"
+  chain  = "srcnat"
+  out_interface = routeros_interface_wireguard.backdoor.name
+}
+
+resource "routeros_ipv6_firewall_nat" "backdoorNATv6" {
+  action = "masquerade"
+  chain = "srcnat"
+  out_interface = routeros_interface_wireguard.backdoor.name
+}
