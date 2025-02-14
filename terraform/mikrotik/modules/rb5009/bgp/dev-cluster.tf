47a58d3751b4554ec5806d9dfea05ef8ec7ca54c
Skyler Mäntysaari <sm+git@skym.fi>
2025-02-14 23:08:57 +0200

    dev bgp

--- terraform/mikrotik/modules/rb5009/bgp/dev-cluster.tf
+++ terraform/mikrotik/modules/rb5009/bgp/dev-cluster.tf
@@ -25,7 +25,7 @@ resource "routeros_routing_bgp_connection" "dev-m1-v6" {
     address = "fd9d:7a72:44eb:d::1"
   }
   remote {
-    address = "fd9d:7a72:44eb:d:e65f:1ff:fe1b:3298"
+    address = "fd9d:7a72:44eb:d:da3a:ddff:fe9f:7977"
     as = routeros_routing_bgp_template.default.as
   }
 }
@@ -47,19 +47,19 @@ resource "routeros_routing_bgp_connection" "dev-m1-v6" {
 #   }
 # }
 
-resource "routeros_routing_bgp_connection" "dev-w2-v6" {
-  templates = [routeros_routing_bgp_template.default.name]
-  as   = routeros_routing_bgp_template.default.as
-  name = "dev-w2-v6"
-  address_families = "ip, ipv6"
-  keepalive_time = "30s"
-  hold_time = "90s"
-  local {
-    role = "ibgp"
-    address = "fd9d:7a72:44eb:d::1"
-  }
-  remote {
-    address = "fd9d:7a72:44eb:d:e65f:1ff:fe1b:3293"
-    as = routeros_routing_bgp_template.default.as
-  }
-}
+# resource "routeros_routing_bgp_connection" "dev-w2-v6" {
+#   templates = [routeros_routing_bgp_template.default.name]
+#   as   = routeros_routing_bgp_template.default.as
+#   name = "dev-w2-v6"
+#   address_families = "ip, ipv6"
+#   keepalive_time = "30s"
+#   hold_time = "90s"
+#   local {
+#     role = "ibgp"
+#     address = "fd9d:7a72:44eb:d::1"
+#   }
+#   remote {
+#     address = "fd9d:7a72:44eb:d:e65f:1ff:fe1b:3293"
+#     as = routeros_routing_bgp_template.default.as
+#   }
+# }
